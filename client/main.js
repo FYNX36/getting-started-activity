e5ae343735923b3174ab14e8874cfc95d8b227ab
F Y N X <69476594+FYNX36@users.noreply.github.com>
2024-03-22 20:18:27 +0100

    1

--- client/main.js
+++ client/main.js
@@ -1,9 +1,14 @@
-import './style.css'
-import rocketLogo from '/rocket.png'
+import { DiscordSDK } from "@discord/embedded-app-sdk";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <img src="${rocketLogo}" class="logo" alt="Discord" />
-    <h1>Hello, World!</h1>
-  </div>
-`;
+import "./style.css";
+import rocketLogo from '/rocket.png';
+
+// Instantiate the SDK
+const discordSdk = new DiscordSDK(import.meta.env.VITE_DISCORD_CLIENT_ID);
+
+await discordSdk.ready();
+// Once the sdk has established the connection with the discord client, external
+// links can be launched
+discordSdk.commands.openExternalLink({
+  url: 'https://google.com',
+});
